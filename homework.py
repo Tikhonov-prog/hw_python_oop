3441a916bf9ba3f5df879c01300699ad34144be0
Vasyliy <belkafavorit91@gmail.com>
2023-05-20 14:06:14 +0900

    review v.3

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
-from dataclasses import dataclass, asdict
-from typing import Union
+import dataclasses
+from dataclasses import asdict,dataclass
 
 
 @dataclass
@@ -11,27 +11,28 @@ class InfoMessage:
     distance: float
     speed: float
     calories: float
-    message = ('Тип тренировки: {};'
-               ' Длительность: {:.3f} ч.;'
-               ' Дистанция: {:.3f} км;'
-               ' Ср. скорость: {:.3f} км/ч;'
-               ' Потрачено ккал: {:.3f}.')
+    MESSAGE = ('Тип тренировки: {training_type};'
+               ' Длительность: {duration:.3f} ч.;'
+               ' Дистанция: {distance:.3f} км;'
+               ' Ср. скорость: {speed:.3f} км/ч;'
+               ' Потрачено ккал: {calories:.3f}.')
+
 
     def get_message(self) -> str:
         """Получить сообщение о тренировке"""
-        return self.message.format(*asdict(self).values())
+        return self.MESSAGE.format(**asdict(self))
 
 
 @dataclass
 class Training:
     """Базовый класс тренировки."""
 
-    LEN_STEP = 0.65
-    M_IN_KM = 1000
-    MIN_IN_HOUR = 60
     action: int
     duration: float
     weight: float
+    LEN_STEP = 0.65
+    M_IN_KM = 1000
+    MIN_IN_HOUR = 60
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
@@ -43,17 +44,16 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        raise NotImplementedError
+        raise NotImplementedError('В классе Training не переопределен'
+                                  ' метод get_spent_calories')
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        info_message = InfoMessage(type(self).__name__,
-                                   self.duration,
-                                   self.get_distance(),
-                                   self.get_mean_speed(),
-                                   self.get_spent_calories()
-                                   )
-        return info_message
+        return InfoMessage(type(self).__name__,
+                           self.duration,
+                           self.get_distance(),
+                           self.get_mean_speed(),
+                           self.get_spent_calories())
 
 
 @dataclass
@@ -77,12 +77,13 @@ class Running(Training):
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
 
+    height: float
     SPORTS_WALKING_CALORIES_MULTIPIER = 0.035
     SPORTS_WALKING_CALORIES_MULTIPIER_2 = 0.029
     KM_IN_H_TO_M_IN_SEC = 0.278
     CM_TO_M = 100
     SQR = 2
-    height: float
+
 
     def get_spent_calories(self) -> float:
         return ((self.SPORTS_WALKING_CALORIES_MULTIPIER
@@ -101,11 +102,12 @@ class SportsWalking(Training):
 class Swimming(Training):
     """Тренировка: плавание."""
 
+    length_pool: float
+    count_pool: int
     LEN_STEP = 1.38
     SWIMING_MULTIPLICATE = 1.1
     SWIMING_MULTIPLICATE_2 = 2
-    length_pool: float
-    count_pool: int
+
 
     def get_spent_calories(self) -> float:
         return ((self.get_mean_speed()
@@ -121,20 +123,20 @@ class Swimming(Training):
                 / self.duration)
 
 
-def read_package(workout_type: str, data: Union[int, float]) -> Training:
+def read_package(workout_type: str, data: list[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
 
-    TYPES_OF_TRAINING: dict[str, tuple[type[Training], int]] = {
-        'SWM': (Swimming, 5),
-        'RUN': (Running, 3),
-        'WLK': (SportsWalking, 4)
+    TYPES_OF_TRAINING: dict[str, type[Training]] = {
+        'SWM': Swimming,
+        'RUN': Running,
+        'WLK': SportsWalking
     }
 
     if workout_type not in TYPES_OF_TRAINING:
-        raise ValueError('Некорректное имя')
-    training_class, num_in_packages = TYPES_OF_TRAINING[workout_type]
-    if len(data) != num_in_packages:
-        raise ValueError('Некорректное число данных')
+        raise ValueError(f'Некорректный код: {workout_type}')
+    training_class = TYPES_OF_TRAINING[workout_type]
+    if len(data) != len(dataclasses.fields(training_class)):
+        raise ValueError(f'Некорректная длина данных {len(data)}')
     return training_class(*data)
 
 
@@ -151,5 +153,4 @@ if __name__ == '__main__':
     ]
 
     for workout_type, data in packages:
-        training = read_package(workout_type, data)
-        main(training)
+        main(read_package(workout_type, data))
